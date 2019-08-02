0018e136eee42fe6ba50e3b7ddcfdc456a41584a
errnull <errnull.top@gmail.com>
2019-08-02 18:33:39 +0800

    fix: Correct file tag des hit target.

--- Source/SVGAVideoEntity.m
+++ Source/SVGAVideoEntity.m
@@ -140,7 +140,7 @@ static NSCache *videoCache;
         if (fileName != nil) {
             NSString *filePath = [self.cacheDir stringByAppendingFormat:@"/%@.png", fileName];
             if (![[NSFileManager defaultManager] fileExistsAtPath:filePath]) {
-               filePath = [self.cacheDir stringByAppendingFormat:@"/%@", fileName];
+                filePath = [self.cacheDir stringByAppendingFormat:@"/%@", fileName];
             }
             if ([[NSFileManager defaultManager] fileExistsAtPath:filePath]) {
                 NSData *imageData = [NSData dataWithContentsOfFile:filePath];
@@ -154,11 +154,11 @@ static NSCache *videoCache;
         }
         else if ([protoImages[key] isKindOfClass:[NSData class]]) {
             NSData *fileTag = [protoImages[key] subdataWithRange:NSMakeRange(0, 4)];
-            if (![[fileTag description] isEqualToString:@"<89504e47>"]) {
+            NSString *fileTagDes = [fileTag description];
+            if (![fileTagDes containsString:@"89504e47"]) {
                 // mp3
                 [audiosData setObject:protoImages[key] forKey:key];
-            }
-            else {
+            } else {
                 UIImage *image = [[UIImage alloc] initWithData:protoImages[key] scale:2.0];
                 if (image != nil) {
                     [images setObject:image forKey:key];
@@ -181,7 +181,7 @@ static NSCache *videoCache;
     }];
     self.sprites = sprites;
 }
-    
+
 - (void)resetAudiosWithProtoObject:(SVGAProtoMovieEntity *)protoObject {
     NSMutableArray<SVGAAudioEntity *> *audios = [[NSMutableArray alloc] init];
     NSArray *protoAudios = [protoObject.audiosArray copy];
